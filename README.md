68bb35fc4533e5e443b221824fe17c3f19726fb5
Thomas Crescenzi <trescenzi@gmail.com>
2020-04-14 01:28:06 +0000

    add macro support

--- README.md
+++ README.md
@@ -7,11 +7,36 @@ footprint, while still being customizable and handling most complex die rolls.
 
 ## Commands
 
+### Rolling Dice
+
 `dice-n-easy` uses [Dice-Bag](//github.com/syntruth/Dice-Bag) for all its die rolling. Some common examples below:
 - Advantage: `/r 2d20k1+5`
 - Disadvantage: `/r 2d20d1+5`
 - Reroll 1s: `/r 1d6 r1`
 - Explode 5s and 6s: `/r 1d6 e5`
+
+### Macros
+
+`dice-n-easy` supports simple custom macros. Macros are user specific, and they
+do care about nicknames. Users can have the same macro names so long as they
+aren't nicknamed the same thing. In order to use macros you need to have a redis
+db hooked up via environment variables as described below. If you don't provide
+a redis db that's ok, you can still use the rolling features just not the
+macros.
+
+#### Adding Macros
+
+Macros are added with `/am <macro name> <dice roll>`. The macro name cannot have
+spaces and are case insensitive, the dice roll can however. Examples:
+- `/am attack 1d20 + 5`
+- `/am advantage_attack 2d20k1 + 5`
+
+#### Using Macros
+
+Macros are used with `/m <macro name>`. They cannot be combined with modifiers.
+Examples:
+- `/m attack`
+- `/m advantage_attack`
 
 ## Serving
 
@@ -20,9 +45,11 @@ on a personal server. All that's required is to set the token environment
 variable. However if you'd like you can customize what the bot looks for in
 commands. The following variables are available:
 - `DICENEASY_TOKEN`: The bot's token
+- `DICENEASY_REDISHOST`: The hostname of the redis db for storing macros.
+  Defaults to `nil`. If not provided, macros won't work. Everything else will
+  though.
+- `DICENEASY_REDISPASSWORD`: The password for the redis db.
 - `DICENEASY_PREFIX`: The prefix for commands. Defaults to `/`
 - `DICENEASY_ROLLCOMMAND`: The command for rolling dice. Defaults to `r`
-
-## TOOD
-
-- [ ] Add redis interface for macros
+- `DICENEASY_ADDMACROCOMMAND`: The command for adding macros. Defaults to `am`
+- `DICENEASY_USEMACROCOMMAND`: The command for using macros. Defaults to `m`
--- dice-n-easy.rb
+++ dice-n-easy.rb
@@ -10,37 +10,91 @@ def check_user_or_nick(event)
   end
 end
 
+def roll_dice(dice_string)
+  puts "Rolling #{dice_string}"
+  dice   = DiceBag::Roll.new(dice_string)
+  dice.result()
+end
+
+def format_dice_result(result, user)
+  reason = ""
+  result.each do |section|
+    reason = "%s %sd%s:%s".% [reason, section.count, section.sides, section.tally] if defined? section.count
+  end
+  response = "#{user} Rolled: `#{result}` \n Breakdown: #{reason}"
+  puts "Formatted dice response #{response}"
+  response 
+end
+
 Dotenv.load
 PREFIX = ENV['DICENEASY_PREFIX'] || '/'
 ROLLCOMMAND = ENV['DICENEASY_ROLLCOMMAND'] || 'r'
-STRIPPREFIX = "#{PREFIX}#{ROLLCOMMAND}"
+REDIS_HOST = ENV['DICENEASY_REDISHOST']
+REDIS_PASSWORD = ENV['DICENEASY_REDISPASSWORD']
+ADD_MACRO_COMMAND = ENV['DICENEASY_ADDMACROCOMMAND'] || 'am'
+USE_MACRO_COMMAND = ENV['DICENEASY_USEMACROCOMMAND'] || 'm'
+TOKEN = ENV['DICENEASY_TOKEN']
 puts "Using command prefix: %s" % PREFIX
 puts "Using roll-command: %s" % ROLLCOMMAND
-puts "Example roll: #{STRIPPREFIX} 1d6+3"
-puts "Using token: %s" % ENV['DICENEASY_TOKEN']
+puts "Example roll: #{PREFIX}#{ROLLCOMMAND} 1d6+3"
+puts "Example add macro: #{PREFIX}#{ADD_MACRO_COMMAND} attack 1d20+4"
+puts "Example use macro: #{PREFIX}#{USE_MACRO_COMMAND} attack"
+puts "Using redis #{REDIS_HOST} for macros" if REDIS_HOST
+puts "Using a password for redis" if REDIS_HOST and REDIS_PASSWORD
+puts "Using token: %s" % TOKEN.slice(-5,5)
+
 @bot = Discordrb::Commands::CommandBot.new token: ENV['DICENEASY_TOKEN'], compress_mode: :large, ignore_bots: true, fancy_log: true, prefix: PREFIX
 @bot.gateway.check_heartbeat_acks = false
 
 @bot.command ROLLCOMMAND.to_sym do |event|
-    puts "#{event.content}"
-    input = event.content
+    puts "ROLLING: #{event.content}"
+    input = event.content.strip
     user = check_user_or_nick(event)
+    dice_prefix = "#{PREFIX}#{ROLLCOMMAND}"
     
-    dice_string = input.delete_prefix(STRIPPREFIX).strip
-    puts "#{user} is rolling #{dice_string}"
+    dice_string = input.delete_prefix(dice_prefix).strip
     begin
-      dice   = DiceBag::Roll.new(dice_string)
-      result = dice.result()
-      reason = ""
-      result.each do |section|
-        reason = "%s %sd%s:%s".% [reason, section.count, section.sides, section.tally] if defined? section.count
-      end
-      response = "#{user} Rolled: `#{result}` \n Breakdown: #{reason}"
-      puts response
-      event.respond response
+      event.respond format_dice_result(roll_dice(dice_string), user)
     rescue
-      event.respond "#{user} oops! #{dice_string} doesn't compute. Please try again"
+      puts "Dice roll #{dice_string} failed"
+      event.respond "#{user} oops! #{dice_string} doesn't compute. Please try again. Common dice rolls look like:\n- Advantage: `#{dice_prefix} 2d20k1+5`\n- Disadvantage: `#{dice_prefix} 2d20d1+5`\n- Reroll 1s: `#{dice_prefix} 1d6 r1`\n- Explode 5s and 6s: `#{dice_prefix} 1d6 e5`"
     end
 end
 
+if REDIS_HOST
+  require 'redis'
+  redis = Redis.new(host: REDIS_HOST, password: REDIS_PASSWORD)
+
+  @bot.command ADD_MACRO_COMMAND.to_sym do |event|
+    puts "ADD MACRO: #{event.content}"
+    input = event.content.strip
+    macro_name = input.split[1].upcase
+    dice_string = input.split.drop(2).join(' ')
+    user = check_user_or_nick(event)
+    begin
+      roll_dice(dice_string)
+      key = "#{user}:#{macro_name}"
+      redis.set(key, dice_string)
+      event.respond "#{user} added macro #{input.split[1]} as #{dice_string}. Use is with #{PREFIX}#{USE_MACRO_COMMAND} #{input.split[1]}"
+    rescue
+      puts "Add macro failed #{input}"
+      event.respond "#{user} oops! #{input} isn't a macro. Add macros look like: #{PREFIX}#{ADD_MACRO_COMMAND} <macro name> <dice to roll>. Please make sure the dice command is valid and there are no spaces in the macro name."
+    end
+  end
+
+  @bot.command USE_MACRO_COMMAND.to_sym do |event|
+    puts "USE MACRO:#{event.content}"
+    input = event.content.strip
+    macro_name = input.split[1].upcase
+    user = check_user_or_nick(event)
+    begin
+      key = "#{user}:#{macro_name}"
+      dice_string = redis.get(key)
+      event.respond format_dice_result(roll_dice(dice_string), user)
+    rescue
+      puts "Use macro failed #{input}"
+      event.respond "#{user} oops! #{input} isn't a saved macro. Remember macros are user and nickname specific."
+    end
+  end
+end
 @bot.run
